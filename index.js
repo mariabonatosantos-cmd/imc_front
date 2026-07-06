8877dd643c5884ef66019d6b01ff44246e56386c
JuliaSkraba <juliaskraba@gmail.com>
2026-07-06 09:33:41 -0300

    edit

--- index.js
+++ index.js
@@ -174,3 +174,29 @@ async function buscarEndereco() {
       console.error("Erro:", error);
     });
 }
+async function cadastrarUsuario() {
+    const dados = {
+        nome: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        senha: document.getElementById("senha").value
+    }
+    try {
+        const res = await fetch("http://localhost:3000/usuarios", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dados)
+        });
+
+        const resultado = await res.json();
+        if (resultado.token) {
+            localStorage.setItem("token", resultado.token);
+            window.location.href = "index.html";
+        } else {
+            alert("Cadastro inválido!");
+        }
+    } catch (erro) {
+        alert("Falha na comunicação com o servidor.");
+    }
+}
